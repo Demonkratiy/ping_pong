1d1a9143c6c2c3e6e64c60097eb323a223e742df
Demonkratiy <demonkratiy@gmail.com>
2020-07-13 00:06:01 +0300

    Updates

---
+++ precache-manifest.fd8c7b413d0f7893727601a3af18f541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e88dd95e778ab53b61c7c34d4824217",
+    "url": "/ping_pong/index.html"
+  },
+  {
+    "revision": "72a0d117df0bffb199a6",
+    "url": "/ping_pong/static/css/main.c4022c09.chunk.css"
+  },
+  {
+    "revision": "d4b1bdbfb391cc073ff2",
+    "url": "/ping_pong/static/js/2.03855407.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ping_pong/static/js/2.03855407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a0d117df0bffb199a6",
+    "url": "/ping_pong/static/js/main.641ee556.chunk.js"
+  },
+  {
+    "revision": "b66110255592446e0b5b",
+    "url": "/ping_pong/static/js/runtime-main.dcf8f968.js"
+  }
+]);
